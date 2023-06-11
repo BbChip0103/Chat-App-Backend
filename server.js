5e580a0e432a950e0ab1a0d644c5a90e4c7c77b8
BbChip0103 <bbchip13@gmail.com>
2023-06-11 12:35:08 +0000

    Solve bug related on socket id

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.on("connection", async (socket) => {
 
   if (user_id != null && Boolean(user_id)) {
     try {
-      User.findByIdAndUpdate(user_id, {
+      await User.findByIdAndUpdate(user_id, {
         socket_id: socket.id,
         status: "Online",
       });
